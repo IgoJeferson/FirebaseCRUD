079e23abb716a1ad244cfcbfba3ef59537bd36d1
logonrm <logonrm@fiap.com.br>
2017-08-01 21:48:23 -0300

    First commit

---
+++ app/src/main/java/br/com/igojeferson/firebasecrud/model/Tarefa.java
@@ -0,0 +1,21 @@
+package br.com.igojeferson.firebasecrud.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Tarefa {
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
